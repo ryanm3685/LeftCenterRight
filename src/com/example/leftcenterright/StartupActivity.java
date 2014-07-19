0a1415618147837c265350550f15b23c91350595
Ryan McGregor <ryanm3685@gmail.com>
2014-07-19 18:45:19 -0400

    got the correct class added.  Intent works.
    Intent is working, for going from start (name entry) screen to main
    screen.

---
+++ src/com/example/leftcenterright/StartupActivity.java
@@ -0,0 +1,98 @@
+package com.example.leftcenterright;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class StartupActivity extends Activity {
+	TextView howMany;
+	EditText numberPlayers;
+	TextView whoIs;
+	EditText playerNameField;
+	Button OKButton;
+	LCRGame game;
+	int n, i;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_startup);
+		
+		
+		
+		howMany = (TextView)findViewById(R.id.HowManyLabel);
+		numberPlayers = (EditText)findViewById(R.id.numberPlayers);
+		whoIs = (TextView)findViewById(R.id.whoIsLabel);
+		playerNameField = (EditText)findViewById(R.id.playerNameField);
+		OKButton = (Button)findViewById(R.id.OKButton);
+		
+		
+		
+		
+		//start by having whoIs and playerNameField invisible until
+		//we've gotten the number of players
+		whoIs.setVisibility(View.INVISIBLE);
+		playerNameField.setVisibility(View.INVISIBLE);
+		
+		//first time OK is clicked
+		OKButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				String s = (numberPlayers.getText()).toString();
+				n = Integer.parseInt(s);
+				game = new LCRGame(n);
+				
+				//now make the invisible stuff visible, and "how many players stuff" invisible
+				howMany.setVisibility(View.INVISIBLE);
+				numberPlayers.setVisibility(View.INVISIBLE);
+				whoIs.setVisibility(View.VISIBLE);
+				playerNameField.setVisibility(View.VISIBLE);
+				i = 0;//index for which player we are adding
+				
+				//now have the OK button add player names
+				OKButton.setOnClickListener(new OnClickListener(){
+						
+					@Override
+					public void onClick(View v) {
+						
+						// TODO Auto-generated method stub
+						String s = (playerNameField.getText()).toString();
+						game.addPlayer(s);
+						if (i == (n - 1))
+						{
+							Intent introIntent = new Intent(getApplicationContext(), IntroActivity.class);
+							startActivity(introIntent);
+						}
+						else
+						{
+							i++;
+							playerNameField.setText(" ");//reset it for entering the other players
+						}		
+						
+					}
+					
+					
+				});
+			}
+			
+		});
+	}
+
+
+
+
+	public void addPlayer()
+	{
+		
+	}
+
+}
