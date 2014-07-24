1dd74fa95d87fc443d42f771742db6c66acdcd04
Ryan McGregor <ryanm3685@gmail.com>
2014-07-24 10:57:38 -0400

    display scores
    The bones of displaying player scores next to their names after the
    round is there.  However, I need to find out why I’m getting whacked
    out values for the scores.

--- src/com/example/leftcenterright/ScoreActivity.java
+++ src/com/example/leftcenterright/ScoreActivity.java
@@ -4,16 +4,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.app.Activity;
 import android.content.Intent;
 
 public class ScoreActivity extends Activity{
 	Button ContinueButton;
+	TextView nameTable, scoreTable;
+	LCRGame game;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_score);
+		
+		//unpack game object sent from startup screen intent
+		game = (LCRGame)getIntent().getSerializableExtra("a_game");
+		nameTable = (TextView)findViewById(R.id.nameTable);//theTable is where scores are displayed
+		scoreTable = (TextView)findViewById(R.id.scoreTable);//theTable is where scores are displayed
+
+		
+		String nameString = "";
+		String scoreString = "";
+		//traverse through every player in the game, and populate the table
+		LCRPlayer p = game.getHeadPlayer();
+		do 
+		{
+			nameString += p.getName() + "\n";
+			scoreString += p.getScore() + "\n";
+			
+			p = p.getNextPlayer();
+		}while (p != game.getHeadPlayer());
+		
+		nameTable.setText(nameString);
+		scoreTable.setText(scoreString);
 		
 		ContinueButton = (Button)findViewById(R.id.ContinueButton);
 		ContinueButton.setOnClickListener(new OnClickListener(){
@@ -25,6 +49,7 @@ public class ScoreActivity extends Activity{
 			}
 			
 		});
+		
 	}
 	
 }
