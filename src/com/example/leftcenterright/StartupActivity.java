013771f5c548f32a892a7481d897c2872ab77c89
Ryan McGregor <ryanm3685@gmail.com>
2014-08-19 11:16:48 -0400

    finished adding in dialogs
    Users enter in number of players and names in dialog boxes instead of
    right inside of the activity

--- src/com/example/leftcenterright/StartupActivity.java
+++ src/com/example/leftcenterright/StartupActivity.java
@@ -18,27 +18,25 @@ import android.widget.TextView;
 public class StartupActivity extends Activity {
 	TextView howMany;
 	EditText numberPlayers;
-	TextView whoIs;
+	TextView whoIsLabel;
 	EditText playerNameField;
 	Button playerOKButton, howManyOKButton, newGameButton, loadGameButton;
 	LCRGame game;
 	int n, i;
 	public  final static String SER_KEY = "com.example.leftcenterright.ser";
-	Context theContext = this;
+	Context theContext;
+	LayoutInflater li;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_startup);
 		
-		
+		theContext = this;
+		li = LayoutInflater.from(theContext);
 		
 		howMany = (TextView)findViewById(R.id.HowManyLabel);
-		numberPlayers = (EditText)findViewById(R.id.numberPlayers);
-		whoIs = (TextView)findViewById(R.id.whoIsLabel);
-		playerNameField = (EditText)findViewById(R.id.playerNameField);
-		playerOKButton = (Button)findViewById(R.id.playerOKButton);
-		howManyOKButton = (Button)findViewById(R.id.howManyOKButton);
+		
 		newGameButton = (Button)findViewById(R.id.newGameButton);
 		loadGameButton = (Button)findViewById(R.id.loadGameButton);
 		
@@ -50,9 +48,10 @@ public class StartupActivity extends Activity {
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				LayoutInflater li = LayoutInflater.from(theContext);
+				
 				View howManyView = li.inflate(R.layout.how_many, null);
- 
+				numberPlayers = (EditText)howManyView.findViewById(R.id.numberPlayers);
+				
 				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
 						theContext);
  
@@ -64,7 +63,18 @@ public class StartupActivity extends Activity {
 				.setPositiveButton("OK",
 				  new DialogInterface.OnClickListener() {
 				    public void onClick(DialogInterface dialog,int id) {
-
+				    	
+				    	
+				    	String s = (numberPlayers.getText()).toString();
+				    	System.out.println(s);
+						n = Integer.parseInt(s);
+						//make sure the value is between 2-7 inclusive
+						if (n < 2) n = 2;
+						if (n > 7) n = 7;
+						
+						game = new LCRGame(n);
+						i = 0;//index for which player we are adding
+				    	getPlayerNames();//this is where the dialog for entering the player names will be
 				    }
 				  })
 				.setNegativeButton("Cancel",
@@ -80,52 +90,58 @@ public class StartupActivity extends Activity {
 			// show it
 			alertDialog.show();
 				
-	
-				
-				//now have the OK button add player names
-			/*	howManyOKButton.setOnClickListener(new OnClickListener(){
-						
-					@Override
-					public void onClick(View v) {
-						
-						String s = (numberPlayers.getText()).toString();
-						n = Integer.parseInt(s);
-						//make sure the value is between 2-7 inclusive
-						if (n < 2) n = 2;
-						if (n > 7) n = 7;
-						
-						game = new LCRGame(n);
-						
-						//fill in player number for who is label
-						whoIs.setText("Who is player " + (i + 1));
-
-						i = 0;//index for which player we are adding
-						
-						
-						String s1 = (playerNameField.getText()).toString();
-						game.addPlayer(s1);
-						if (i == (n - 1))//got everyone's name
-						{
-							Intent introIntent = new Intent(getApplicationContext(), IntroActivity.class);
-							Bundle introBundle = new Bundle();
-							introBundle.putSerializable("the_game", game);
-							introIntent.putExtras(introBundle);
-							startActivity(introIntent);
-						}
-						else
-						{
-							i++;
-							playerNameField.setText(" ");//reset it for entering the other players
-							whoIs.setText("Who is player " + (i + 1));
-						}		
-						
-					}
-					
-					
-				});*/
 			}
 			
 		});
+	}
+	
+	public void getPlayerNames()
+	{
+		View playerDialogView = li.inflate(R.layout.player_dialog, null);
+		whoIsLabel = (TextView)playerDialogView.findViewById(R.id.whoIsLabel); 
+		playerNameField = (EditText)playerDialogView.findViewById(R.id.playerNameField);
+		whoIsLabel.setText("Who is player " + (i + 1));
+		
+		
+		AlertDialog.Builder playerDialogBuilder = new AlertDialog.Builder(
+				theContext);
+
+		playerDialogBuilder.setView(playerDialogView);
+		
+		playerDialogBuilder
+		.setCancelable(false)
+		.setPositiveButton("OK",
+		  new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog,int id) {
+		    	//todo:  fill this bad boy in!
+		    	//fill in player number for who is label
+				whoIsLabel.setText("Who is player " + (i + 1));
+				
+				String s1 = (playerNameField.getText()).toString();
+				game.addPlayer(s1);
+				if (i == (n - 1))//got everyone's name
+				{
+					Intent introIntent = new Intent(getApplicationContext(), IntroActivity.class);
+					Bundle introBundle = new Bundle();
+					introBundle.putSerializable("the_game", game);
+					introIntent.putExtras(introBundle);
+					startActivity(introIntent);
+				}
+				else
+				{
+					i++;
+					playerNameField.setText(" ");//reset it for entering the other players
+					
+					getPlayerNames();
+				}		
+		    }
+		  });
+
+		// create alert dialog
+		AlertDialog playerDialog = playerDialogBuilder.create();
+
+		// show it
+		playerDialog.show();
 	}
 
 }
