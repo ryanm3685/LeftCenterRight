eb0d388fe9f8b84150483113abe8b3e106ee70dd
Ryan McGregor <ryanm3685@gmail.com>
2014-08-19 09:52:30 -0400

    converted "how many" into a dialog.

--- src/com/example/leftcenterright/StartupActivity.java
+++ src/com/example/leftcenterright/StartupActivity.java
@@ -3,7 +3,11 @@ package com.example.leftcenterright;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,10 +20,11 @@ public class StartupActivity extends Activity {
 	EditText numberPlayers;
 	TextView whoIs;
 	EditText playerNameField;
-	Button OKButton;
+	Button playerOKButton, howManyOKButton, newGameButton, loadGameButton;
 	LCRGame game;
 	int n, i;
 	public  final static String SER_KEY = "com.example.leftcenterright.ser";
+	Context theContext = this;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,50 +37,73 @@ public class StartupActivity extends Activity {
 		numberPlayers = (EditText)findViewById(R.id.numberPlayers);
 		whoIs = (TextView)findViewById(R.id.whoIsLabel);
 		playerNameField = (EditText)findViewById(R.id.playerNameField);
-		OKButton = (Button)findViewById(R.id.OKButton);
+		playerOKButton = (Button)findViewById(R.id.playerOKButton);
+		howManyOKButton = (Button)findViewById(R.id.howManyOKButton);
+		newGameButton = (Button)findViewById(R.id.newGameButton);
+		loadGameButton = (Button)findViewById(R.id.loadGameButton);
 		
-		
-		
-		
-		//start by having whoIs and playerNameField invisible until
-		//we've gotten the number of players
-		whoIs.setVisibility(View.INVISIBLE);
-		playerNameField.setVisibility(View.INVISIBLE);
+
 		
 		//first time OK is clicked
-		OKButton.setOnClickListener(new OnClickListener(){
+		newGameButton.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				String s = (numberPlayers.getText()).toString();
-				n = Integer.parseInt(s);
-				//make sure the value is between 2-7 inclusive
-				if (n < 2) n = 2;
-				if (n > 7) n = 7;
+				LayoutInflater li = LayoutInflater.from(theContext);
+				View howManyView = li.inflate(R.layout.how_many, null);
+ 
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+						theContext);
+ 
+				// set prompts.xml to alertdialog builder
+				alertDialogBuilder.setView(howManyView);
 				
-				game = new LCRGame(n);
+				alertDialogBuilder
+				.setCancelable(false)
+				.setPositiveButton("OK",
+				  new DialogInterface.OnClickListener() {
+				    public void onClick(DialogInterface dialog,int id) {
+
+				    }
+				  })
+				.setNegativeButton("Cancel",
+				  new DialogInterface.OnClickListener() {
+				    public void onClick(DialogInterface dialog,int id) {
+					dialog.cancel();
+				    }
+				  });
+
+			// create alert dialog
+			AlertDialog alertDialog = alertDialogBuilder.create();
+
+			// show it
+			alertDialog.show();
 				
-				//now make the invisible stuff visible, and "how many players stuff" invisible
-				howMany.setVisibility(View.INVISIBLE);
-				numberPlayers.setVisibility(View.INVISIBLE);
-				
-				//fill in player number for who is label
-				whoIs.setText("Who is player " + (i + 1));
-				
-				whoIs.setVisibility(View.VISIBLE);
-				playerNameField.setVisibility(View.VISIBLE);
-				i = 0;//index for which player we are adding
+	
 				
 				//now have the OK button add player names
-				OKButton.setOnClickListener(new OnClickListener(){
+			/*	howManyOKButton.setOnClickListener(new OnClickListener(){
 						
 					@Override
 					public void onClick(View v) {
 						
+						String s = (numberPlayers.getText()).toString();
+						n = Integer.parseInt(s);
+						//make sure the value is between 2-7 inclusive
+						if (n < 2) n = 2;
+						if (n > 7) n = 7;
 						
-						String s = (playerNameField.getText()).toString();
-						game.addPlayer(s);
+						game = new LCRGame(n);
+						
+						//fill in player number for who is label
+						whoIs.setText("Who is player " + (i + 1));
+
+						i = 0;//index for which player we are adding
+						
+						
+						String s1 = (playerNameField.getText()).toString();
+						game.addPlayer(s1);
 						if (i == (n - 1))//got everyone's name
 						{
 							Intent introIntent = new Intent(getApplicationContext(), IntroActivity.class);
@@ -94,7 +122,7 @@ public class StartupActivity extends Activity {
 					}
 					
 					
-				});
+				});*/
 			}
 			
 		});
